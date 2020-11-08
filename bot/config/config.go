a92b544ef3225cdaefc7697255bcaaf7b0409837
Matthias Dötsch <matze@mdoetsch.de>
2020-11-08 21:25:02 +0100

    added github actions

--- bot/config/config.go
+++ bot/config/config.go
@@ -2,13 +2,14 @@ package config
 
 // Config contains the full config structure of this bot
 type Config struct {
-	Slack       Slack     `yaml:"slack"`
-	Server      Server    `yaml:"server"`
-	Jenkins     Jenkins   `yaml:"jenkins"`
-	Jira        Jira      `yaml:"jira"`
-	Mqtt        Mqtt      `yaml:"mqtt"`
-	StoragePath string    `yaml:"storage_path"`
-	Bitbucket   Bitbucket `yaml:"bitbucket"`
+	Slack Slack `mapstructure:"slack"`
+	// todo move
+	Server      Server    `mapstructure:"server"`
+	Jenkins     Jenkins   `mapstructure:"jenkins"`
+	Jira        Jira      `mapstructure:"jira"`
+	Mqtt        Mqtt      `mapstructure:"mqtt"`
+	StoragePath string    `mapstructure:"storage_path"`
+	Bitbucket   Bitbucket `mapstructure:"bitbucket"`
 	Github      struct {
 		AccessToken string
 	}
@@ -25,13 +26,13 @@ type Config struct {
 	BranchLookup struct {
 		Type       string // stash/bitbucket/git/null
 		Repository string
-	} `yaml:"branch_lookup"`
+	} `mapstructure:"branch_lookup"`
 
-	AllowedUsers []string    `yaml:"allowed_users,flow"`
-	AdminUsers   []string    `yaml:"admin_users,flow"`
-	OpenWeather  OpenWeather `yaml:"open_weather"`
-	PullRequest  PullRequest `yaml:"pullrequest"`
-	Timezone     string      `yaml:"timezone"`
+	AllowedUsers []string    `mapstructure:"allowed_users,flow"`
+	AdminUsers   []string    `mapstructure:"admin_users,flow"`
+	OpenWeather  OpenWeather `mapstructure:"open_weather"`
+	PullRequest  PullRequest `mapstructure:"pullrequest"`
+	Timezone     string      `mapstructure:"timezone"`
 }
 
 // OpenWeather is an optional feature to get current weather
@@ -44,20 +45,25 @@ type OpenWeather struct {
 
 // Slack contains the credentials and configuration of the Slack client
 type Slack struct {
-	Token            string   `yaml:"token"`
-	Debug            bool     `yaml:"debug"`
-	AllowedGroups    []string `yaml:"allowed_groups,flow"`
-	AutoJoinChannels []string `yaml:"auto_join_channels,flow"`
-	ErrorChannel     string   `yaml:"error_channel"`
+	Token            string   `mapstructure:"token"`
+	Debug            bool     `mapstructure:"debug"`
+	AllowedGroups    []string `mapstructure:"allowed_groups,flow"`
+	AutoJoinChannels []string `mapstructure:"auto_join_channels,flow"`
+	ErrorChannel     string   `mapstructure:"error_channel"`
+
+	// use websocket RTM connection.
+	// this is NOT possible anymore for new apps!
+	UseRTM      bool `mapstructure:"use_rtm"`
+	UseEventAPI bool `mapstructure:"use_event_api"`
 
 	// only used for integration tests
-	TestEndpointUrl   string `yaml:"-"`
-	VerificationToken string `yaml:"-"`
+	TestEndpointUrl   string `mapstructure:"-"`
+	VerificationToken string `mapstructure:"-"`
 }
 
 type Server struct {
-	Listen             string `yaml:"listen"`
-	VerificationSecret string `yaml:"verification_secret"`
+	Listen             string `mapstructure:"listen"`
+	VerificationSecret string `mapstructure:"verification_secret"`
 }
 
 func (c Server) IsEnabled() bool {
@@ -65,8 +71,8 @@ func (c Server) IsEnabled() bool {
 }
 
 type Logger struct {
-	Level string `yaml:"level"`
-	File  string `yaml:"file"`
+	Level string `mapstructure:"level"`
+	File  string `mapstructure:"file"`
 }
 
 // Macro represents a single macro which is defined by a trigger regexp and a list of executed commands
@@ -92,17 +98,17 @@ func (c Mqtt) IsEnabled() bool {
 // PullRequest special configuration to change the pull request behavior
 type PullRequest struct {
 	// able to set a custom "approved" reactions to see directly who or which component/department approved a pullrequest
-	CustomApproveReaction map[string]string `yaml:"custom_approve_reaction"`
+	CustomApproveReaction map[string]string `mapstructure:"custom_approve_reaction"`
 }
 
 // Bitbucket credentials/options. Either add Username+Password OR a ApiKey
 type Bitbucket struct {
-	Host       string `yaml:"host"`
-	Username   string `yaml:"username"`
-	Password   string `yaml:"password"`
-	ApiKey     string `yaml:"api_key"`
-	Project    string `yaml:"project"`
-	Repository string `yaml:"repository"`
+	Host       string `mapstructure:"host"`
+	Username   string `mapstructure:"username"`
+	Password   string `mapstructure:"password"`
+	ApiKey     string `mapstructure:"api_key"`
+	Project    string `mapstructure:"project"`
+	Repository string `mapstructure:"repository"`
 }
 
 func (c Bitbucket) IsEnabled() bool {
