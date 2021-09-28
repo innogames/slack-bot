77aae8112e88391d1078512a6cca8112d730e6cf
Matthias Dötsch <matthias.doetsch@innogames.com>
2021-09-28 20:53:29 +0200

    optimize locks + http handling (#231)
    * optimize locks + http handling
    
    * optimize locks + http handling

--- bot/lock.go
+++ bot/lock.go
@@ -2,22 +2,26 @@ package bot
 
 import "sync"
 
-var globalLock sync.Mutex
+var (
+	globalLock sync.Mutex
+	userLocks  = map[string]*sync.Mutex{}
+)
 
 // get and hold a mutex for each requested user.
 // the user-lock it's locked by default
-func (b *Bot) getUserLock(userID string) *sync.Mutex {
+func getUserLock(userID string) *sync.Mutex {
 	var userLock *sync.Mutex
 
 	globalLock.Lock()
-	userLock, ok := b.userLocks[userID]
+	defer globalLock.Unlock()
+
+	userLock, ok := userLocks[userID]
 	if !ok {
 		userLock = &sync.Mutex{}
-		b.userLocks[userID] = userLock
+		userLocks[userID] = userLock
 	}
 
 	userLock.Lock()
-	globalLock.Unlock()
 
 	return userLock
 }
