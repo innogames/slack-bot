ecc17032401813aa7ab621df3c34e95b764ef3d4
Matthias Dötsch <matthias.doetsch@innogames.com>
2024-06-06 16:24:04 +0200

    WIP: implement hashicorp plugins

--- bot/plugin.go
+++ bot/plugin.go
@@ -1,29 +1,131 @@
 package bot
 
 import (
+	"fmt"
+	"github.com/hashicorp/go-hclog"
+	plugin "github.com/hashicorp/go-plugin"
 	"github.com/innogames/slack-bot/v2/client"
+	log "github.com/sirupsen/logrus"
+	"net/rpc"
+	"os"
+	"os/exec"
 )
 
-// Plugin is a extended command which can be registered to the bot at compile time
-type Plugin struct {
-	Init func(bot *Bot, slackClient client.SlackClient) Commands
+type SlackBotPluginI interface {
+	GetCommands(cfg *Bot, slack client.SlackClient) Commands
+}
+type SlackBotPlugin struct {
+	GetCommands func(cfg *Bot, slack client.SlackClient) Commands
 }
 
-var pluginList []Plugin
-
-// RegisterPlugin registers a new plugin, also in init() time
-func RegisterPlugin(plugin Plugin) {
-	pluginList = append(pluginList, plugin)
-}
-
-func loadPlugins(bot *Bot, slackClient client.SlackClient) Commands {
+func LoadPlugins(b *Bot) Commands {
 	commands := Commands{}
 
-	for _, plugin := range pluginList {
-		commands.Merge(plugin.Init(bot, slackClient))
+	for _, pluginPath := range b.config.Plugins {
+		log.Infof("Load plugin %s...", pluginPath)
+
+		c, err := loadPlugin(pluginPath)
+		fmt.Println(err)
+		c.GetCommands()
 	}
 
 	clear(pluginList)
 
 	return commands
+}
+
+func loadPlugin(pluginPath string) (*CommandRPC, error) {
+	// todo use only one logger, and use logrus
+	logger := hclog.New(&hclog.LoggerOptions{
+		Name:   "plugin",
+		Output: os.Stdout,
+		Level:  hclog.Debug,
+	})
+
+	client := plugin.NewClient(&plugin.ClientConfig{
+		HandshakeConfig: plugin.HandshakeConfig{
+			ProtocolVersion:  1,
+			MagicCookieKey:   "COMMAND_PLUGIN",
+			MagicCookieValue: "bar",
+		},
+		Plugins: map[string]plugin.Plugin{
+			"command": &SlackBotPlugin{},
+		},
+		Cmd:    exec.Command(pluginPath),
+		Logger: logger,
+	})
+	defer client.Kill()
+
+	rpcClient, err := client.Client()
+	if err != nil {
+		return nil, err
+	}
+
+	raw, err := rpcClient.Dispense("command")
+	if err != nil {
+		return nil, err
+	}
+
+	return raw.(*CommandRPC), nil
+}
+
+// ServePlugin serves the plugin
+func ServePlugin(impl *SlackBotPlugin) {
+	logger := hclog.New(&hclog.LoggerOptions{
+		Name:   "plugin",
+		Output: os.Stdout,
+		Level:  hclog.Debug,
+	})
+
+	fmt.Println("start serving plugin")
+	plugin.Serve(&plugin.ServeConfig{
+		HandshakeConfig: plugin.HandshakeConfig{
+			ProtocolVersion:  1,
+			MagicCookieKey:   "COMMAND_PLUGIN",
+			MagicCookieValue: "bar",
+		},
+		Plugins: map[string]plugin.Plugin{
+			"command": impl,
+		},
+		//GRPCServer: plugin.DefaultGRPCServer,
+		Logger: logger,
+	})
+	fmt.Println("end serving plugin")
+}
+
+// Plugin Server Implementation
+func (p *SlackBotPlugin) Server(*plugin.MuxBroker) (any, error) {
+	return &CommandRPCServer{Impl: p}, nil
+	// todo? return &GreeterRPCServer{Impl: p.Impl, b: b}, nil
+}
+
+// Plugin Client Implementation
+func (p *SlackBotPlugin) Client(b *plugin.MuxBroker, c *rpc.Client) (any, error) {
+	return &CommandRPC{client: c}, nil
+}
+
+// Concrete implementation of the plugin Command that wraps RPC client.
+type CommandRPC struct {
+	client *rpc.Client
+}
+
+func (g *CommandRPC) GetCommands() {
+	var resp string
+	err := g.client.Call("Plugin.GetCommands", new(any), &resp)
+	fmt.Println(err)
+	fmt.Println(resp)
+	if err != nil {
+		panic(err)
+	}
+}
+
+type CommandRPCServer struct {
+	// This is the real implementation
+	Impl *SlackBotPlugin
+}
+
+func (s *CommandRPCServer) GetCommands(args interface{}, resp *string) error {
+	commands := s.Impl.GetCommands(&Bot{}, &client.Slack{})
+	fmt.Println(commands)
+	return nil
 }
---
+++ plugins/test/go.mod
@@ -0,0 +1,61 @@
+module github.com/innogames/slack-bot/v2/plugins/test
+
+go 1.22
+
+replace github.com/innogames/slack-bot/v2 => ../../
+
+require (
+	github.com/hashicorp/go-plugin v1.6.1
+	github.com/innogames/slack-bot/v2 v2.0.0-00010101000000-000000000000
+)
+
+require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/brainexe/viper v1.8.2 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/fatih/color v1.14.1 // indirect
+	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/gfleury/go-bitbucket-v1 v0.0.0-20240131155556-0b41d7863037 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/hackebrot/turtle v0.2.0 // indirect
+	github.com/hashicorp/go-hclog v1.5.0 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/hashicorp/yamux v0.1.1 // indirect
+	github.com/jcelliott/lumber v0.0.0-20160324203708-dd349441af25 // indirect
+	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/mitchellh/go-testing-interface v1.0.0 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/oklog/run v1.0.0 // indirect
+	github.com/pelletier/go-toml v1.2.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/prometheus/client_golang v1.19.1 // indirect
+	github.com/prometheus/client_model v0.5.0 // indirect
+	github.com/prometheus/common v0.48.0 // indirect
+	github.com/prometheus/procfs v0.12.0 // indirect
+	github.com/redis/go-redis/v9 v9.5.1 // indirect
+	github.com/rifflock/lfshook v0.0.0-20180920164130-b9218ef580f5 // indirect
+	github.com/sdomino/golang-scribble v0.0.0-20230717151034-b95d4df19aa8 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/slack-go/slack v0.13.0 // indirect
+	github.com/spf13/afero v1.11.0 // indirect
+	github.com/spf13/cast v1.6.0 // indirect
+	github.com/spf13/jwalterweatherman v1.0.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/subosito/gotenv v1.6.0 // indirect
+	github.com/texttheater/golang-levenshtein/levenshtein v0.0.0-20200805054039-cae8b0eaed6c // indirect
+	golang.org/x/exp v0.0.0-20240506185415-9bf2ced13842 // indirect
+	golang.org/x/net v0.20.0 // indirect
+	golang.org/x/oauth2 v0.20.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
+	golang.org/x/text v0.15.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20231120223509-83a465c0220f // indirect
+	google.golang.org/grpc v1.59.0 // indirect
+	google.golang.org/protobuf v1.33.0 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
